dc7c9f99044edf6e0d81085bed98915776ec1692
Richard Rubin <rrubin98@gmail.com>
2024-09-23 16:44:14 -0400

    Initial Commit

---
+++ FlickrSearchChallenge/FlickrSearchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSearchChallengeApp.swift
+//  FlickrSearchChallenge
+//
+//  Created by Richard B. Rubin on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrSearchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickrSearchChallengeTests/FlickrSearchChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSearchChallengeTests.swift
+//  FlickrSearchChallengeTests
+//
+//  Created by Richard B. Rubin on 9/23/24.
+//
+
+import Testing
+@testable import FlickrSearchChallenge
+
+struct FlickrSearchChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
